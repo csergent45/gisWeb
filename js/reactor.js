f5e60b6127983e2c331b1315880b10a7ee72ce28
Chris Sergent <dsergent@decaturil.gov>
2015-01-09 17:14:53 -0600

    Added measurement widget.

--- js/reactor.js
+++ js/reactor.js
@@ -14,23 +14,47 @@
 // Get references to modules to be used
 require(["esri/map",                                // mapSection
          "esri/config",                             // The default values for all JS API configuration options. 
+
+         "esri/Color",  // measurementDiv
+
          "esri/dijit/HomeButton", // Home Button
+         "esri/dijit/Measurement", // measurementDiv
          "esri/dijit/OverviewMap", // Overview Map
          "esri/dijit/Scalebar", // Scalebar
+         
          "esri/geometry/Extent", // The minimum and maximum X- and Y- coordinates of a bounding box. Used to set custom extent
+         
          "esri/layers/ArcGISDynamicMapServiceLayer",
          "esri/layers/ArcGISTiledMapServiceLayer",
+         "esri/layers/LayerDrawingOptions", // measurementDiv
          "esri/layers/FeatureLayer",
+
+         "esri/renderers/SimpleRenderer", // measurementDiv
+
+         "esri/SnappingManager", // measurementDiv    -add snapping capability
+
+         "esri/sniff", // measurementDiv
+
+         "esri/symbols/SimpleFillSymbol", // measurementDiv
+         "esri/symbols/SimpleLineSymbol", // measurementDiv
+         
          "esri/tasks/GeometryService",    // Represents a geometry service resource exposed by the ArcGIS Server REST API.
+
          "dojo/dom",                            // It is used for code like - dom.byId("someNode")
+         "dojo/keys",
          "dojo/on",                             // This module is used based on an even such as on("click")
          "dojo/parser",                         // The Dojo Parser is an optional module.
+
+         "dijit/layout/BorderContainer",
+         "dijit/layout/ContentPane",
+         "dijit/TitlePane",
+         "dijit/form/CheckBox",
          "dojo/domReady!"],    // An AMD loaded plugin that will wait until the DOM has finished loading before returning.
 
 // Set variables to be used with references (write variables and references in the same order and be careful of typos on your references)
-         function (Map, esriConfig, HomeButton, OverviewMap, Scalebar, Extent,
-                   ArcGISDynamicMapServiceLayer, ArcGISTiledMapServiceLayer,
-                   FeatureLayer, GeometryService, dom, on, parser) {
+         function (Map, esriConfig, Color, HomeButton, Measurement, OverviewMap, Scalebar, Extent,
+                   ArcGISDynamicMapServiceLayer, ArcGISTiledMapServiceLayer, LayerDrawingOptions,
+                   FeatureLayer, SimpleRenderer, SnappingManager, has, SimpleFillSymbol, SimpleLineSymbol, GeometryService, dom, keys, on, parser) {
 
              parser.parse();
 
@@ -116,6 +140,46 @@ require(["esri/map",                                // mapSection
                  scalebarUnit: "dual"
              });
              // scalebar End
+
+             // start measurement tool - the current layer we are measuring is the operational layer
+
+             // defining the lines that will be drawn for measurement
+             var layerDrawingOptions = [];
+             var layerDrawingOption = new LayerDrawingOptions();
+             var sfs = new SimpleFillSymbol(
+                                    "solid",
+                                    new SimpleLineSymbol("solid", new Color([195, 176, 23]), 2),
+                                    null
+                                    );
+
+
+             layerDrawingOption.renderer = new SimpleRenderer(sfs);
+
+             // change 1 to the layer index that you want to modify:
+             layerDrawingOptions[1] = layerDrawingOption;
+
+
+             //dojo.keys.copyKey maps to CTRL on windows and Cmd on Mac., but has wrong code for Chrome on Mac
+             var snapManager = map.enableSnapping({
+                 snapKey: has("mac") ? keys.META : keys.CTRL
+             });
+
+             // layer used for measuring tool. Your tool wont' show up without it.
+             var layerInfos = [{
+                 layer: operationalLayer
+             }];
+
+             // enables snapping
+             snapManager.setLayerInfos(layerInfos);
+
+             // looks for the domID of measurementDiv and starts the measurement tool there
+             var measurement = new Measurement({
+                 map: map
+             }, dom.byId("measurementDiv"));
+             measurement.startup();
+
+
+             // end measurement tool
 
 
          });
