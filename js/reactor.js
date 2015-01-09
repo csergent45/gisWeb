962d7c9ee3682d122f0ddee11637b8572756c359
Chris Sergent <dsergent@decaturil.gov>
2015-01-09 09:04:02 -0600

    Added scalebar.

--- js/reactor.js
+++ js/reactor.js
@@ -16,6 +16,7 @@ require(["esri/map",                                // mapSection
          "esri/config",                             // The default values for all JS API configuration options. 
          "esri/dijit/HomeButton",                   // homeButton
          "esri/dijit/OverviewMap", // Overview Map
+         "esri/dijit/Scalebar",  // Scalebar
          "esri/geometry/Extent",                    // The minimum and maximum X- and Y- coordinates of a bounding box. Used to set custom extent
          "esri/layers/ArcGISDynamicMapServiceLayer",
          "esri/layers/ArcGISTiledMapServiceLayer",
@@ -28,7 +29,7 @@ require(["esri/map",                                // mapSection
 
 
 // Set variables to be used with references (write variables and references in the same order and be careful of typos on your references)
-         function (Map, esriConfig, HomeButton, OverviewMap, Extent,
+         function (Map, esriConfig, HomeButton, OverviewMap, Scalebar, Extent,
                    ArcGISDynamicMapServiceLayer, ArcGISTiledMapServiceLayer,
                    FeatureLayer, GeometryService, dom, on, parser) {
 
@@ -98,17 +99,23 @@ require(["esri/map",                                // mapSection
 
              // add homeButton begin
              var home = new HomeButton({
-                 map: map,
+                 map: map
              }, "homeButton");
              home.startup();
              // add homeButton end
 
              // overviewMap Begin
              var overviewMapDijit = new OverviewMap({
-                map:map,
-                visible:false
-            });
-            overviewMapDijit.startup();
+                 map: map,
+                 visible: false
+             });
+             overviewMapDijit.startup();
              // overviewMap End
 
+             // scalebar Begin
+             var scalebar = new Scalebar({
+                 map: map,
+                 scalebarUnit: "dual"
+             });
+             // scalebar End
          });
