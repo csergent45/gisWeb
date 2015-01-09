a41125bf5f3075844d878027021d08beafa9d8a7
Chris Sergent <dsergent@decaturil.gov>
2015-01-09 11:48:56 -0600

    Fixed scalebar.

--- js/reactor.js
+++ js/reactor.js
@@ -3,30 +3,29 @@
 //
 //   Author: Chris Sergent
 //   Date:   January 6, 2014
-//   Application: GIS Web
+//   Application: GIS Mobile
 //
 ////////////////////////////////////////////////////////////////////////////////////
 
-var map, geoLocate;
 
-// Comments describing require statements are definition from https://developers.arcgis.com/javascript/jsapi/ and http://dojotoolkit.org/reference-guide/1.9/
+// Comments describing require statements are definition from https://developers.arcgis.com/javascript/jsapi/ 
+// and http://dojotoolkit.org/reference-guide/1.9/
 
 // Get references to modules to be used
 require(["esri/map",                                // mapSection
          "esri/config",                             // The default values for all JS API configuration options. 
-         "esri/dijit/HomeButton",                   // homeButton
+         "esri/dijit/HomeButton", // Home Button
          "esri/dijit/OverviewMap", // Overview Map
-         "esri/dijit/Scalebar",  // Scalebar
-         "esri/geometry/Extent",                    // The minimum and maximum X- and Y- coordinates of a bounding box. Used to set custom extent
+         "esri/dijit/Scalebar", // Scalebar
+         "esri/geometry/Extent", // The minimum and maximum X- and Y- coordinates of a bounding box. Used to set custom extent
          "esri/layers/ArcGISDynamicMapServiceLayer",
          "esri/layers/ArcGISTiledMapServiceLayer",
          "esri/layers/FeatureLayer",
-         "esri/tasks/GeometryService",          // Represents a geometry service resource exposed by the ArcGIS Server REST API. It is used to perform various operations on geometries such as project, simplify, buffer, and relationships. 
-         "dojo/dom",                            // This module defines the core Dojo DOM API. It is used for code like - dom.byId("someNode")
-         "dojo/on",                             // This module is used based on an even such as on("click"), function() { } or on("ready"),function() { }
-         "dojo/parser",                         // The Dojo Parser is an optional module which is used to convert specially decorated nodes in the DOM and convert them into Dijits, Widgets or other Objects.
-         "dojo/domReady!"],                     // An AMD loaded plugin that will wait until the DOM has finished loading before returning.
-
+         "esri/tasks/GeometryService",    // Represents a geometry service resource exposed by the ArcGIS Server REST API.
+         "dojo/dom",                            // It is used for code like - dom.byId("someNode")
+         "dojo/on",                             // This module is used based on an even such as on("click")
+         "dojo/parser",                         // The Dojo Parser is an optional module.
+         "dojo/domReady!"],    // An AMD loaded plugin that will wait until the DOM has finished loading before returning.
 
 // Set variables to be used with references (write variables and references in the same order and be careful of typos on your references)
          function (Map, esriConfig, HomeButton, OverviewMap, Scalebar, Extent,
@@ -37,7 +36,8 @@ require(["esri/map",                                // mapSection
 
              /* The proxy comes before all references to web services */
              /* Files required for security are proxy.config, web.config and proxy.ashx 
-             - set security in Manager to Private, available to selected users and select Allow access to all users who are logged in
+             - set security in Manager to Private, available to selected users and select 
+             Allow access to all users who are logged in
              (Roles are not required)
              /*
              Information on the proxy can be found at: https://developers.arcgis.com/javascript/jshelp/ags_proxy.html
@@ -57,8 +57,8 @@ require(["esri/map",                                // mapSection
              //-----------------------------------------------------------
 
              // declare geometry service
-             esriConfig.defaults.geometryService = new GeometryService("http://maps.decaturil.gov/arcgis/rest/services/Utilities/Geometry/GeometryServer");
-
+             esriConfig.defaults.geometryService =
+             new GeometryService("http://maps.decaturil.gov/arcgis/rest/services/Utilities/Geometry/GeometryServer");
 
              // set custom extent
              var initialExtent = new Extent({
@@ -91,8 +91,6 @@ require(["esri/map",                                // mapSection
              var pointFeatureLayer = new FeatureLayer("http://maps.decaturil.gov/arcgis/rest/services/Test/FeatureServer/0");
              map.addLayer(pointFeatureLayer);
 
-
-
              //-----------------------------------------------------------
              // Map Services End
              //-----------------------------------------------------------
@@ -103,7 +101,7 @@ require(["esri/map",                                // mapSection
              }, "homeButton");
              home.startup();
              // add homeButton end
-
+             
              // overviewMap Begin
              var overviewMapDijit = new OverviewMap({
                  map: map,
@@ -118,4 +116,6 @@ require(["esri/map",                                // mapSection
                  scalebarUnit: "dual"
              });
              // scalebar End
+
+
          });
